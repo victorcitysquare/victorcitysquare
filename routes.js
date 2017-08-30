0d06b696b799bd1f95dcf234126707242ecd056a
Mujahid Masood <mujahid.masood@stud.tu-darmstadt.de>
2017-08-30 02:18:00 +0200

    -added route for location details

--- routes.js
+++ routes.js
@@ -21,7 +21,7 @@ module.exports = function (app) {
 
 
     app.get('/location', location.getLocation);  // Get Location
-
+    app.get('/locationdetails', location.getLocationDeatils);
 
     app.post('/uploadImage', fileController.uploadImage);
     app.get('/download', fileController.download);
